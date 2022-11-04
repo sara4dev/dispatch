01cb7143acaeab4aa9d551ba3300939e5de80a98
saravana periyasamy <saperiyasamy@nvidia.com>
2022-11-04 13:10:30 -0500

    added smtp plugin

---
+++ src/dispatch/plugins/dispatch_smtp/config.py
@@ -0,0 +1,20 @@
+from pydantic import Field, SecretStr
+
+from dispatch.config import BaseConfigurationModel
+
+
+class SMTPConfiguration(BaseConfigurationModel):
+    """SMTP configuration"""
+
+    smtp_server: SecretStr = Field(
+        title="SMTP Server",
+        description="SMTP Server address.",
+    )
+    smtp_port: str = Field(
+        title="SMTP Server Port",
+        description="SMTP Server Port, default is 25",
+    )
+    from_email_address: str = Field(
+        title="From email address",
+        description="From email address.",
+    )
